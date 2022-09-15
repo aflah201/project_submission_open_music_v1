9b691e2bdc46a379ac422a7865b5d414b6554059
Aflah Azzaky <aflahazzaki123@gmail.com>
2022-09-16 01:10:35 +0700

    Implementasi fitur autentikasi

--- src/server.js
+++ src/server.js
@@ -7,19 +7,25 @@ const ClientError = require('./exceptions/ClientError');
 const album = require('./api/albums');
 const song = require('./api/songs');
 const user = require('./api/users');
+const authentications = require('./api/authentications');
 // Service
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
 const UsersService = require('./services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 // Validator
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
 const UsersValidator = require('./validator/users');
+const AuthenticationsValidator = require('./validator/authentications');
+// Tokenize
+const TokenManager = require('./tokenize/TokenManager');
 
 const init = async () => {
   const albumService = new AlbumsService();
   const songService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -51,6 +57,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
