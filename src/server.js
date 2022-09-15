80503b5747646dc398bc024b6c3f4c44925b05ef
Aflah Azzaky <aflahazzaki123@gmail.com>
2022-09-15 23:03:43 +0700

    Implementasi fitur user

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+// Custom Error
+const ClientError = require('./exceptions/ClientError');
+// Api
 const album = require('./api/albums');
 const song = require('./api/songs');
-const ClientError = require('./exceptions/ClientError');
+const user = require('./api/users');
+// Service
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
+const UsersService = require('./services/postgres/UsersService');
+// Validator
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumService = new AlbumsService();
   const songService = new SongsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -35,6 +44,13 @@ const init = async () => {
       options: {
         service: songService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: user,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
